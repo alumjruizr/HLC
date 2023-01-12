3aa8c677067a2810a3229a8f2bf9bd22180afc53
alumjruizr <alum.jruizr.2021@iesalixar.org>
2023-01-12 21:11:39 +0100

    app t3

--- Prueba/blog/views.py
+++ Prueba/blog/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -8,3 +9,33 @@ def saludo(request):
 
 def despedida(request):
     return HttpResponse("Esta es la página de despedida")
+
+def dameFecha(request):
+    fecha_actual = datetime.datetime.now()
+
+    documento = """
+    <html>
+    <body>
+    <h2>
+    La fecha y hora actual es: {}
+    </h2>
+    </body>
+    </html>
+    """.format(fecha_actual)
+
+    return HttpResponse(documento)
+
+def calculaEdadActual(request, edad, agno):
+    periodo = agno - datetime.datetime.now().year 
+    nueva_edad = edad + periodo
+    documento = """
+      <html>
+      <body>
+      <h2>
+      En el año {} tendrás: {}
+      </h2>
+      </body>
+      </html>
+      """.format(agno, nueva_edad)
+
+    return HttpResponse(documento)
--- Prueba/mi_proyecto/urls.py
+++ Prueba/mi_proyecto/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import saludo, despedida
+from blog.views import saludo, despedida, calculaEdadActual
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
     path('adios/', despedida),
+    path('edad/<int:edad>/<int:agno>/', calculaEdadActual),   
     ]
